3c52e9c8782c4346ecdd4d61975a5eab2c6a8512
nngocgioi2712 <nguyenngocgioi127@gmail.com>
2024-06-23 19:50:58 +0700

    change softAssert

--- src/main/java/com/vmo/demowebshop/pageobject/HomePageObject.java
+++ src/main/java/com/vmo/demowebshop/pageobject/HomePageObject.java
@@ -6,10 +6,7 @@ import com.vmo.demowebshop.interfaces.HomePageUI;
 import com.vmo.demowebshop.storedata.AddToCartData;
 import com.vmo.demowebshop.utils.Log;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-
-import java.util.List;
 
 public class HomePageObject extends BasePage {
     private WebDriver driver;
@@ -44,16 +41,13 @@ public class HomePageObject extends BasePage {
 
     public HeaderObject addFeaturedItemsToShoppingCart(int number) {
         Log.allure("Step: Add %s Featured Items To Shopping Cart", Integer.toString(number));
-        List<WebElement> items = getListWebElements(driver, HomePageUI.FEATURED_PRODUCT_NAME);
-        if(items.size() < number) {
-            throw new RuntimeException("The total items is not enough " + number);
-        } else {
-            AddToCartData.setNumberAdded(Integer.parseInt(getTextElement(driver, CommonPageUI.SHOPPING_CART_ITEMS_TOTAL).replaceAll("[^0-9]", "")));
-            for (int i = 0; i < number; i++) {
-                items.get(i).click();
-                clickToElement(driver, CommonPageUI.DYNAMIC_INPUT_BTN, "Add to cart");
-                backToPage(driver);
-            }
+        AddToCartData.setNumberAdded(Integer.parseInt(getTextElement(driver, CommonPageUI.SHOPPING_CART_ITEMS_TOTAL).replaceAll("[^0-9]", "")));
+        for (int i = 0; i < number; i++) {
+            clickToElement(driver, HomePageUI.FEATURED_PRODUCT_NAME);
+            clickToElement(driver, CommonPageUI.DYNAMIC_INPUT_BTN, "Add to cart");
+            waitForElementVisible(driver, CommonPageUI.NOTIFICATION_MSG);
+            softAssert.assertEquals(getTextElement(driver, CommonPageUI.NOTIFICATION_MSG), "The product has been added to your shopping cart");
+            backToPage(driver);
         }
         refreshCurrentPage(driver);
         return new HeaderObject(driver);
